08d4100cb6ee5d86fefbe304bc5f14fc0547231d
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-12-04 16:48:32 +0300

    add db commands

--- src/main/java/org/example/demo/database/schema.sql
+++ src/main/java/org/example/demo/database/schema.sql
@@ -1,3 +1,8 @@
+CREATE USER buildog WITH PASSWORD 'your-beautiful-password';
+CREATE DATABASE buildog;
+GRANT ALL PRIVILEGES ON DATABASE buildog TO buildog;
+
+
 CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
